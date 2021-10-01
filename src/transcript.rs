08dc58389cb68d6b262ab1e3e2e5c01f2419fdb6
Jules de Smit <julesdesmit@gmail.com>
2021-10-01 15:15:41 +0200

    Refactor transcript file to use arkworks backend

--- src/transcript.rs
+++ src/transcript.rs
@@ -6,37 +6,36 @@
 
 //! This is an extension over the [Merlin Transcript](Transcript)
 //! which adds a few extra functionalities.
+use ark_ec::{PairingEngine, CanonicalSerialize};
 use crate::commitment_scheme::kzg10::Commitment;
-use dusk_bls12_381::BlsScalar;
-use dusk_bytes::Serializable;
 use merlin::Transcript;
 
 /// Transcript adds an abstraction over the Merlin transcript
 /// For convenience
-pub(crate) trait TranscriptProtocol {
+pub(crate) trait TranscriptProtocol<E: PairingEngine> {
     /// Append a `commitment` with the given `label`.
     fn append_commitment(&mut self, label: &'static [u8], comm: &Commitment);
 
     /// Append a `BlsScalar` with the given `label`.
-    fn append_scalar(&mut self, label: &'static [u8], s: &BlsScalar);
+    fn append_scalar(&mut self, label: &'static [u8], s: &E::Fr);
 
     /// Compute a `label`ed challenge variable.
-    fn challenge_scalar(&mut self, label: &'static [u8]) -> BlsScalar;
+    fn challenge_scalar(&mut self, label: &'static [u8]) -> E::Fr;
 
     /// Append domain separator for the circuit size.
     fn circuit_domain_sep(&mut self, n: u64);
 }
 
-impl TranscriptProtocol for Transcript {
+impl<E: PairingEngine> TranscriptProtocol<E> for Transcript {
     fn append_commitment(&mut self, label: &'static [u8], comm: &Commitment) {
         self.append_message(label, &comm.0.to_bytes());
     }
 
-    fn append_scalar(&mut self, label: &'static [u8], s: &BlsScalar) {
+    fn append_scalar(&mut self, label: &'static [u8], s: &E::Fr) {
         self.append_message(label, &s.to_bytes())
     }
 
-    fn challenge_scalar(&mut self, label: &'static [u8]) -> BlsScalar {
+    fn challenge_scalar(&mut self, label: &'static [u8]) -> E::Fr {
         let mut buf = [0u8; 64];
         self.challenge_bytes(label, &mut buf);
 
