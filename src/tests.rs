c8ef3ccbd6b56067835a4e10c9894455430b6dab
David Nevado <david.nevado@iohk.io>
2021-11-15 18:20:38 +0100

    Add support for panic tests in `batch_test` macro

--- src/tests.rs
+++ src/tests.rs
@@ -3,17 +3,24 @@ mod test {
     /// Run a set of tests on a pairing engine / curve combination
     #[macro_export]
     macro_rules! batch_test {
-        ( [$($test_set:ident),+] => ($engine:ty, $curve:ty, $params:ty) ) => {
+        ( [$($test_set:ident),*], [$($test_panic_set:ident),*] => ($engine:ty, $curve:ty, $params:ty) ) => {
             paste::item! {
                 $(
-                #[test]
-                #[allow(non_snake_case)]
-                fn [< $test_set _on_ $engine>]() {
-                    $test_set::<$engine, $curve, $params>()
-                }
-                    )+
+                    #[test]
+                    #[allow(non_snake_case)]
+                    fn [< $test_set _on_ $engine>]() {
+                        $test_set::<$engine, $curve, $params>()
+                    }
+                )*
+                $(
+                    #[test]
+                    #[should_panic]
+                    #[allow(non_snake_case)]
+                    fn [< $test_panic_set _on_ $engine>]() {
+                        $test_panic_set::<$engine, $curve, $params>()
+                    }
+                )*
             }
         }
-
     }
 }
