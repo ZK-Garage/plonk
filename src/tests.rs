f9d8e069210cc24784cb40ddb9d61e18bf03868c
David Nevado <david.nevado@iohk.io>
2021-11-16 16:14:31 +0100

    Update `batch_test` doc

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod test {
-    /// Run a set of tests on a pairing engine / curve combination
+    /// Run a set of tests on a pairing engine / curve combination.
+    /// The set of tests is split in two. The first set between []
+    /// is for regular tests that should not panic. The second set
+    /// is for tests that should panic.
     #[macro_export]
     macro_rules! batch_test {
         ( [$($test_set:ident),*], [$($test_panic_set:ident),*] => ($engine:ty, $params:ty) ) => {
