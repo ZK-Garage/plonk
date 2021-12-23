a01d3a8b7de96776edd7f2feabf16f6289e603aa
Luke Pearson <pearsonluke73@gmail.com>
2021-12-23 15:48:43 +0000

    Include `src/lib.rs`

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// Copyright (c) ZK-GARAGE. All rights reserved.
+
+//! PLONK
+
+#![cfg_attr(not(any(feature = "std", test)), no_std)]
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
+#![forbid(rustdoc::broken_intra_doc_links)]
+#![forbid(missing_docs)]
+
+#[doc(inline)]
+pub use plonk_core::*;
+
+#[doc(inline)]
+pub use plonk_hashing as hashing;
