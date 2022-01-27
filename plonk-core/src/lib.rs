f6a504cf2270f630d28e4d1641100cda2a34558c
Luke Pearson <pearsonluke73@gmail.com>
2022-01-26 22:31:18 -0500

    Remove doc links

--- plonk-core/src/lib.rs
+++ plonk-core/src/lib.rs
@@ -49,14 +49,4 @@ pub mod proof_system;
 #[cfg(test)]
 mod test;
 
-#[doc = include_str!("../../docs/notes-intro.md")]
-pub mod notes {
-    #[doc = include_str!("../../docs/notes-commitments.md")]
-    pub mod commitment_schemes {}
-    #[doc = include_str!("../../docs/notes-snark.md")]
-    pub mod snark_construction {}
-    #[doc = include_str!("../../docs/notes-prove-verify.md")]
-    pub mod prove_verify {}
-    #[doc = include_str!("../../docs/notes-KZG10.md")]
-    pub mod kzg10 {}
-}
+
