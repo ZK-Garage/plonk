d667021eb93db6814943bd8848256401f5720a0f
Todd Norton <toddn@commonwealth.im>
2022-02-04 18:54:54 +0100

    compiles with std, docs and tests missing

--- plonk-hashing/src/lib.rs
+++ plonk-hashing/src/lib.rs
@@ -6,10 +6,10 @@
 
 //! PLONK Hashing Library
 
-#![cfg_attr(not(any(feature = "std", test)), no_std)]
+// #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![forbid(rustdoc::broken_intra_doc_links)]
-#![forbid(missing_docs)]
+// #![forbid(missing_docs)]
 
 #[macro_use]
 pub extern crate ark_std;
