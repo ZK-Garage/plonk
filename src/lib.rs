1fa720a6b0f2fe9b7f9f008b2a1b81550922c386
David Nevado <david.nevado@iohk.io>
2022-03-21 13:36:11 +0100

    Fix `src/lib.rs`
    Old module organization removed

--- src/lib.rs
+++ src/lib.rs
@@ -10,32 +10,8 @@
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![forbid(rustdoc::broken_intra_doc_links)]
 #![forbid(missing_docs)]
-
 #[doc(inline)]
 pub use plonk_core::*;
 
-mod permutation;
-mod transcript;
-mod util;
-
-pub mod circuit;
-pub mod constraint_system;
-pub mod error;
-pub mod lookup;
-pub mod prelude;
-pub mod proof_system;
-
-#[cfg(test)]
-mod test;
-
-#[doc = include_str!("../docs/notes-intro.md")]
-pub mod notes {
-    #[doc = include_str!("../docs/notes-commitments.md")]
-    pub mod commitment_schemes {}
-    #[doc = include_str!("../docs/notes-snark.md")]
-    pub mod snark_construction {}
-    #[doc = include_str!("../docs/notes-prove-verify.md")]
-    pub mod prove_verify {}
-    #[doc = include_str!("../docs/notes-KZG10.md")]
-    pub mod kzg10_docs {}
-}
+#[doc(inline)]
+pub use plonk_hashing as hashing;
